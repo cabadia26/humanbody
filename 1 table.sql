1d9e0cbf61c5ce2543f84614d81d06cee75f09ef
cabadia26 <cabadia26@gmail.com>
2024-02-28 10:38:48 -0500

    1-change table

--- 1 table.sql
+++ 1 table.sql
@@ -1,5 +1,5 @@
 use humanbodydb
 go 
-create table dbo.body(id int not null primary key,
+create table dbo.body(id int not null identity primary key,
  bodypart varchar(100) not null, 
  num int not null)
