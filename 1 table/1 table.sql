c241a88a12a1a054dd6a5d6c9bb3ec2b5e3748db
cabadia26 <cabadia26@gmail.com>
2024-03-01 05:31:18 -0500

    1st commit trial branch

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -2,7 +2,7 @@ use humanbodydb
 go 
 drop table if exists body 
 create table dbo.body(id int not null identity primary key,
---creaturetype varchar(25) not null, 
+creaturetype varchar(25) not null, 
  bodypart varchar(100) not null, 
  num int null
  )
